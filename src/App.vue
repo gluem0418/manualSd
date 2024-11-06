fce0da527220c15d4c6cd8871cec32ec2c5d672c
gluem <dayes3010@gmail.com>
2024-11-06 09:41:46 +0900

    add all

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TitleFlame from '@/components/TitleFlame.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <!-- <header> -->
+  <TitleFlame class="title" :inside="'Manual'" />
+  <!-- </header> -->
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  margin: 20px auto;
+  width: 100%;
+}
+
+.title {
+  margin: auto;
+  display: block;
+  /* margin: 50px auto; */
+  /* width: 30%; */
+}
+</style>
---
+++ src/components/TitleFlame.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flame">{{ inside }}</div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  inside: { type: String },
+});
+
+</script>
+<style scoped>
+.flame {
+  width: 500px;
+  background-image: url('@/assets/img/flame1.png');
+  background-size: 100% 100%;
+  text-align: center;
+  padding: 3.8vh 2vw;
+  font-family: 'Aboreto';
+  color: #F5F5F5;
+  font-size: 48px;
+}
+</style>
